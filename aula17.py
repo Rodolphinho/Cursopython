2b4417198042966618396996adc086abfbdd3be1
Rodolphinho <140680092+Rodolphinho@users.noreply.github.com>
2023-10-03 02:31:05 -0300

    Add files via upload
    matematica e if elif else...

---
+++ aula17.py
@@ -0,0 +1,54 @@
+#aula eif elif else
+
+
+
+#entrada = input ('voce quer entrar ou sair?')
+
+#if entrada == 'entrar':
+    #print('voce entrou no sistema')
+
+
+#elif entrada == 'sair':
+    #print ('voce saiu')
+#else:
+    #print('vc nao entrou nem saiu bobao')
+    #
+
+
+
+
+nome = 'Rodolpho'
+sobrenome = 'Souza'
+idade= 22
+ano_nascimento = 2023 - idade
+peso = 65
+altura_metros = 1.67
+
+maior_idade = idade >= 18
+
+imc = peso // altura_metros ** 2 
+
+
+adicao = 10 + 10
+subtracao = 15 - 10
+divisao = 4 / 2
+potencia = 10 ** 2
+multi = 2 * 2
+
+
+
+print ( 'nome:', nome)
+print ('sobrenome:', sobrenome)
+print ('idade', idade)
+print ('ano de nascimento', ano_nascimento)
+print ('altura em metros', altura_metros)
+print ('seu imc é', imc)
+print ('é de maior?', maior_idade)
+
+entrada = input ('voce quer entrar ou sair?')
+if entrada == 'entrar':
+    print('Você acabou de adentrar o programa.',)
+elif entrada == 'sair':
+    print('você desligou o programa.',)
+
+   
